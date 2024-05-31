0a80b39c002ec44406f352205ae45bbfe5de7f85
kolobita <166148931+kolobita@users.noreply.github.com>
2024-05-30 22:57:44 -0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { TiendaComponent } from './pages/tienda/tienda.component';
+import { CompanyComponent } from './pages/company/company.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", redirectTo:'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'nosotros', component: NosotrosComponent},
+  {path: 'servicios', component: ServiciosComponent},
+  {path: 'tienda', component: TiendaComponent},
+  {path: 'company', component: CompanyComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: '**', redirectTo: 'home', pathMatch: 'full'},
+];
